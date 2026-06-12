5d73a7b13219ca9fec1af52c257dbf34ae223c34
Sunny <kaulikdas2017@gmail.com>
2026-06-12 11:17:06 +0530

    sfd bmd added and 2 fos added as inputs

---
+++ main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Fix for __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { 
+      nodeIntegration: true,
+      contextIsolation: false // Often required if nodeIntegration is true
+    }
+  });
+
+  win.loadFile('index.html'); 
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
